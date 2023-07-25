915c466ad53d681dfea341d4b3089d89027d842f
sultan aljrboa <sultanaljrboua@gmail.com>
2023-07-25 15:21:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+from food import Food
+from scorebord import Scorebord
+import winsound
+
+winsound.PlaySound("just for fun.wav", winsound.SND_ASYNC + winsound.SND_LOOP)
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scorebord = Scorebord()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        print("nom nomm nomm")
+        food.refresh()
+        snake.extend()
+        scorebord.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scorebord.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scorebord.game_over()
+
+
+
+screen.exitonclick()
